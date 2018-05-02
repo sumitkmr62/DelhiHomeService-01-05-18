863638ccee77419b755303fe547fb57ec810465a
sumit kumar <sumitkmr62@gmail.com>
2018-05-02 11:43:22 +0530

    Commit-02-05-18

---
+++ DelhiHomeService.Data/Admin/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace DelhiHomeService.Data.Admin
+{
+    public class Login
+    {
+        public void GetConnection()
+        {
+            
+        }
+    }
+}
---
+++ DelhiHomeService.Models/Admin/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelhiHomeService.Models.Admin
+{
+    public class Account
+    {
+        public int AdminID { get; set; }
+        public string AdminUsername { get; set; }
+        public string AdminPassword { get; set; }
+    }
+}
